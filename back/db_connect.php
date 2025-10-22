e171d457746f60eb9319778edaa6cc8ba4bb8a8d
rikuto831 <2401138@s.asojuku.ac.jp>
2025-10-22 12:44:44 +0900

    commit

---
+++ back/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql326.phy.lolipop.lan';
+    const DBNAME = 'LAA1607635-hdc';
+    const USER = 'LAA1607635';
+    const PASS = 'team05';
+
+    $connect = 'mysql:host=' . SERVER . ';dbname=' . DBNAME . ';charset=utf8';
+?>
--- back/home.php
+++ back/home.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>back</title>
+    <title>ホーム</title>
 </head>
 <body>
-    <?php
-    echo 'test';
-    ?>   
+        
 </body>
 </html>
