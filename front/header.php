320aa81c23a29a312943599f0b2587fd66dadb8b
2401185 <2401185@s.asojuku.ac.jp>
2025-10-27 12:06:16 +0900

    header、foote、cssの更新

--- front/header.php
+++ front/header.php
@@ -4,6 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HDC ショッピングサイト</title>
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<header>
+        <h2><a href="main.php">icon</a></h2>
+        <nav class="nav">
+            <ul class="ul">
+                <li class="searchList">
+                    <div class="search-inner">
+                    <form action="search.php" method="post">
+                        <input type="text" class="search" placeholder="検索"> 
+                        <button type="submit"><img src="image/search.png" class="searchIcon"></button>
+                    </form>
+                    </div>
+                </li>
+                <div class="RightLi">
+                    <li class="navIcon"><a href="#" class="navLink">出品</a></li>
+                    <li class="navIcon"><a href="#" class="navLink">お知らせ</a></li>
+                    <li class="navIcon"><a href="#" class="navLink"><img src="image/star.png" class="navImage" alt="star">お気に入り</a></li>
+                    <li class="navIcon"><a href="#" class="navLink">カート</a></li>
+                    <li class="navIcon"><a href="#" class="navLink">アカウント</a></li>
+                </div>
+            </ul>
+        </nav>
+    </header>
     
