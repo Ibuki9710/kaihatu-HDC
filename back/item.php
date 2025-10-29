40abba3f2b8fb978dd37c3cff4b7ff718be42877
rikuto831 <2401138@s.asojuku.ac.jp>
2025-10-29 12:29:23 +0900

    Update item.php

--- back/item.php
+++ back/item.php
@@ -1,5 +1,15 @@
 <?php session_start(); ?>
 <?php require 'db_connect.php'; ?>
 <?php
-    
+    $keyword = $_POST['keyword']
+    $pdo=new PDO($connect, USER, PASS);
+    if(isset($keyword)){
+        $sql=$pdo->prepare('select * from item where item_name like ?');
+        $sql->execute(['%'.$keyword.'%']);
+    }else{
+        $sql=$pdo->query('select * from item')
+    }
+    foreach($sql as $row){
+        $id=$row['item_id']
+    }
 ?>
