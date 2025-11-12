7a118831a7237045ec9b1e61f820349a5d74ffce
Ibuki9710 <2401156@s.asojuku.ac.jp>
2025-11-12 10:56:52 +0900

    修正

--- back/login.php
+++ back/login.php
@@ -5,17 +5,17 @@ require_once 'db_connect.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
         $pdo = new PDO($connect, USER, PASS);
-        $stmt = $pdo->prepare("SELECT * FROM customers WHERE email = ?");
+        $stmt = $pdo->prepare("SELECT * FROM customer WHERE member_email = ?");
         $stmt->execute([$_POST['email']]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($user && password_verify($_POST['password'], $user['password'])) {
-            $_SESSION['customer_id'] = $user['customer_id'];
-            header('Location: ../frontend/customer_menu.php');
+            $_SESSION['member_id'] = $user['member_id'];
+            header('Location: ../front/customer_menu.php');
             exit;
         } else {
             $_SESSION['login_error'] = 'メールアドレスまたはパスワードが違います';
-            header('Location: ../frontend/login.php');
+            header('Location: ../front/login.php');
             exit;
         }
 
