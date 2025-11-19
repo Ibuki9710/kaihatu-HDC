888dc20f524f1d3e92c8e6784a736205ab75ff8c
rikuto831 <2401138@s.asojuku.ac.jp>
2025-11-19 12:16:27 +0900

    commit

--- back/cart-delete.php
+++ back/cart-delete.php
@@ -1,7 +1,5 @@
 <?php session_start(); ?>
 <?php
     unset($_SESSION['cart'][$_GET['id']]);
-    echo 'カートから商品を削除しました。';
-    echo '<hr>';
     require 'cart.php';
 ?>
--- back/cart.php
+++ back/cart.php
@@ -3,12 +3,12 @@
     if(!empty($_SESSION['cart'])){
         $total=0;
         foreach($_SESSION['cart'] as $id=>$product){
+            echo '<a href="../back/cart-delete.php?id=', $id, '">×</a>';
             echo '<img src="', $product['img_path'], '">';
             echo '<h3>', $product['item_name']'</h3>';
             echo '<p>価格　　', $product['price'], '円</p>';
             echo '<p>送料　　無料</p>';
             echo '<p>数量　　', $product['count'], '</p>';
-            echo '<a href="#">商品注文画面へ</a>';
         }
     }else{
         echo 'カートに商品がありません。';
