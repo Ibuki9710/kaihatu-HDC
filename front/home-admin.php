d4fdd21eebeeead957dcb46d43cbadc8241f1888
2401185 <2401185@s.asojuku.ac.jp>
2025-11-19 10:59:39 +0900

    修正

--- front/home-admin.php
+++ front/home-admin.php
@@ -1,5 +1,18 @@
-<?php
+<?php 
 require 'header-admin.html';
-require 'list-admin.html';
-require 'footer-admin.html';
+require 'list-admin.html'; 
 ?>
+<div class="conteiner-header">
+    <h2>ホーム</h2>
+</div>
+<div class="admin-main">
+    <div class="conteiner-admin">
+        <p>お知らせ一覧</p>
+    </div>
+    <div class="conteiner-admin">
+        <div class="blue container">
+            <p>安売りセール！</p>
+        </div>
+    </div>
+</div>
+<?php require 'footer.html'; ?>
