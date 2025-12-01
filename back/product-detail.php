60beaa3922fa775bdcf1e6849b912f508e45e17e
2401185 <2401185@s.asojuku.ac.jp>
2025-12-01 12:21:42 +0900

    商品詳細の追加

--- back/product-detail.php
+++ back/product-detail.php
@@ -1,21 +1,25 @@
-<?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php
     $pdo=new PDO($connect, USER, PASS);
     $sql=$pdo->prepare('select * from item where id=?');
     $sql->execute([$_GET['id']]);
     foreach($sql as $row){
-        echo '<p><img alt="image" src="./image/', $row['id'], '.jpg"></p>';
-        echo '<form action="cart-insert.php?id=', $row['id'], '" method="post">';
-        echo '<p>', $row['item_name'], '</p>';
+        echo '<div class="product-header-row">';
+        echo '<div class="product-media-description">';
+        echo '<div class="product-image-area">';
+        echo '<p><img alt="image" src="./image/', $row['id'], '.jpg" class="image"></p>';
+        echo '</div>';
+        echo '<div class="product-detail-summary">';
+        echo '<h2>', $row['item_name'], '</h2>';
         echo '<p>', $row['item_explain'], '</p>';
+        echo '<div class="product-action-box">';
+        echo '<form action="cart-insert.php?id=', $row['id'], '" method="post">';
         echo '<p>￥', $row['price'], '</p>';
         echo '<p>送料無料</p>';
-        echo '数量<input type="number" name="count">';
-        echo '<p><input type="submit" value="カートに追加"></p>';
-        echo '<p>', $row['item_stock'], '</p>';
-        echo '<p>', $row['width'], '</p>';
-        echo '<p>', $row['height'], '</p>';
+        echo '<div class="form-group">';
+        echo '<label>数量</label><input type="number" name="count">';
+        echo '</div>';
+        echo '<button class="btn-base yellowBtn login-btn" style="width: 100%; margin-top: 15px;">カートに追加</button>';
         echo '<input type="hidden" name="id" value="', $row['item_id'], '">';
         echo '<input type="hidden" name="name" value="', $row['item_name'], '">';
         echo '<input type="hidden" name="price" value="', $row['price'], '">';
@@ -26,5 +30,13 @@
         echo '<input type="hidden" name="item_explain" value="', $row['item_explain'], '">';
         echo '<input type="hidden" name="brand" value="', $row['brand'], '">';
         echo '</form>';
+        echo '</div>';
+        echo '</div>';
+        echo '<div class="product-full-description" style="max-width: 1200px; margin: 30px auto;">';
+        echo '<p>', $row['item_stock'], '</p>';
+        echo '<p>', $row['width'], '</p>';
+        echo '<p>', $row['height'], '</p>';
+        echo '</div>';
+        echo '</div>';
     }
 ?>
---
+++ front/detail.php
@@ -0,0 +1,5 @@
+<?php session_start();
+require 'header.html';
+require 'product-detail.php';
+require 'footer.html'; 
+?>
