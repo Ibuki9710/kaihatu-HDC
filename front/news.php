6e8e92f65c3fae5acc1b698bbbc54fcba8ecbb54
Ibuki9710 <2401156@s.asojuku.ac.jp>
2025-12-12 00:57:36 +0900

    Update news.php

--- front/news.php
+++ front/news.php
@@ -12,7 +12,6 @@ require_once __DIR__ . '/../back/db_connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>お知らせ一覧</title>
     <link rel="stylesheet" href="../css/news.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
     <div class="center">
